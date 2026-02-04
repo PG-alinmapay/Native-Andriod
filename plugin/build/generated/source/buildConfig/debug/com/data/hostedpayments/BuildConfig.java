76fb7da5a80f8ec58cae2f17f918f35f69c960a5
Riyaztechsupport <135695828+Riyaztechsupport@users.noreply.github.com>
2026-02-04 14:49:28 +0300

    Add files via upload

---
+++ plugin/build/generated/source/buildConfig/debug/com/data/hostedpayments/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.data.hostedpayments;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.data.hostedpayments";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String SDK_VERSION_CODE = "v3.0.3";
+}
